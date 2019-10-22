a7b255672dca4089cfaf7910df7b433cc37fb464
vpnagvek <vpnagvek@ncsu.edu>
2019-10-22 18:13:02 -0400

    TestCase2 Selenium

---
+++ Testcase2.py
@@ -0,0 +1,47 @@
+from  selenium import webdriver
+from  selenium.common.exceptions import TimeoutException
+import time
+
+def UseCase2(Flow):
+    #if Flow==1:
+    time.sleep(30)
+    lastfield=[]
+    lastfield = browser.find_elements_by_class_name("post-message__text")
+    if "stale" in  (lastfield[len(lastfield)-1].text):
+        print ("Successfully displayed the list")
+    else:
+        print("There are no stale issues")
+        #exit(0)
+
+    if Flow==1:
+        time.sleep(2)
+        ###
+        bottomText = browser.find_element_by_id("post_textbox")
+        bottomText.send_keys("Reassign IssueId 2371 to Jacob")
+        bottomText.submit()
+        time.sleep(1)
+        lastfield=[]
+        lastfield = browser.find_elements_by_class_name("post-message__text")
+        if "reassigned" in  (lastfield[len(lastfield)-1].text):
+            print ("Successfully reassigned issue")
+        else:
+            print("Error while reassigning issue")
+            exit(0)
+
+browser = webdriver.Chrome('/usr/bin/chromedriver')
+browser.get("http://localhost:8065/se-team/messages/@testbot")
+time.sleep(2)
+username = browser.find_element_by_id("loginId")
+password = browser.find_element_by_id("loginPassword")
+submit   = browser.find_element_by_id("loginButton")
+
+username.send_keys("cmanideep96@gmail.com")
+password.send_keys("Manideep@96")
+submit.submit()
+
+Flow=1
+UseCase2(Flow)
+
+Flow=0
+UseCase2(Flow)
+
