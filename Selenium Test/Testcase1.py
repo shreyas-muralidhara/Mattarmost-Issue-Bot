ee74a68f289d83f6ae6ff250bf990ce732df41ee
Sai Manideep Ch <schippa@ncsu.edu>
2019-11-30 21:35:59 -0500

    Jenkins Testing

--- Selenium Test/Testcase1.py
+++ Selenium Test/Testcase1.py
@@ -11,20 +11,19 @@ def UseCase1(Flow):
         bottomText = browser.find_element_by_id("post_textbox")
         bottomText.send_keys("display list of open issues")
         bottomText.submit()
-        time.sleep(1)
+        time.sleep(4)
         lastfield=[]
         lastfield = browser.find_elements_by_class_name("post-message__text")
         if "issues" in  (lastfield[len(lastfield)-1].text):
-            sys.stderr.write("Successfully displaying issues")
-          
+            print("Successfully displaying issues")                
         else:
             print("Error while displaying issues")
             exit(1)
-        time.sleep(1)
+        time.sleep(2)
         bottomText = browser.find_element_by_id("post_textbox")
         bottomText.send_keys("update the priority for #999 to Normal")
         bottomText.submit()
-        time.sleep(1)
+        time.sleep(2)
         lastfield=[]
         lastfield = browser.find_elements_by_class_name("post-message__text")
         if "exist" in  (lastfield[len(lastfield)-1].text):
@@ -33,14 +32,14 @@ def UseCase1(Flow):
            # print("Error")
             print("Error occurred")
             exit(1)   
-        time.sleep(1)
+        time.sleep(2)
         bottomText = browser.find_element_by_id("post_textbox")
         bottomText.send_keys("update status for #51 to implemented")
         bottomText.submit()
-        time.sleep(1)
+        time.sleep(2)
         lastfield=[]
         lastfield = browser.find_elements_by_class_name("post-message__text")
-        if "valid" in  (lastfield[len(lastfield)-1].text):
+        if "Not a valid" in  (lastfield[len(lastfield)-1].text):
             print ("Not a valid Status")
         else:
             print("Error occured")
@@ -48,6 +47,7 @@ def UseCase1(Flow):
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 browser = webdriver.Chrome('/usr/bin/chromedriver',options=chrome_options)
+#browser = webdriver.Chrome('/usr/bin/chromedriver')
 browser.get("http://34.66.158.171:8065/bot/messages/@issuebot")
 
 time.sleep(5)
@@ -55,7 +55,7 @@ username = browser.find_element_by_id("loginId")
 password = browser.find_element_by_id("loginPassword")
 submit   = browser.find_element_by_id("loginButton")
 username.send_keys('sghanta')
-password.send_keys('Issuebot@team11')
+password.send_keys('Issuebot@11')
 submit.submit()
 
 
